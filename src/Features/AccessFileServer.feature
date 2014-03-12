174d95ac5af3f718d2ed62d99ef685eb32ef0cb9
mikesouth <mikesouth@gmail.com>
2014-03-12 19:02:56 -0400

    AccessFileServer.feature update. 2 step defs

--- src/Features/AccessFileServer.feature
+++ src/Features/AccessFileServer.feature
@@ -5,16 +5,15 @@ Feature:
 
   @FileServer
   Scenario: FileShare
-    Given I want to upload a file to the file server
+    Given I am going to upload a file to the file server
     When The file is located at the directory myfile/thisfile
     And The file destination is located at the directory testersfile/thisfile
     And I am a member of the group Testers
-    And I send the file to the file server
-    Then The file shall be added to the file server's database
+    Then The group server shall confirm it recieved the file successfully
     
   @FileServer
   Scenario: FileDownload
-	Given I want to download a file from my group
-	When The file I want to download is groupfiles/thisfile
-	And I am a member of the group Testers
-	Then I shall be able to verify the file is downloaded to my computer
+	Given I am going to download a file from my group
+	When The file I want to download is testersfile/thisfile
+	And I want to download the file to myfile/thisfile
+	Then I shall be able to confirm the file is downloaded to my computer
--- src/Testing/FileShareStepDefinitions.java
+++ src/Testing/FileShareStepDefinitions.java
@@ -19,35 +19,31 @@ public class FileShareStepDefinitions{
 	@Mock
 	FileClient m_fc; 
 	
-	@Given("I wish to upload a file")
+	@Given("I want to upload a file to the file server")
 	public void a_user_shares_a_file(){
 		m_fc = Mockito.mock(FileClient.class);
 	}
 	
-	@When("The file is located at .+")
+	@When("The file is located at the directory (.+)$")
 	public void set_file_location(){
 		file_loc = "myfile/thisfile";
 	}
 	
-	@And("The destination is .+")
+	@And("The file destination is located at the directory (.+)$")
 	public void set_file_destination(){
 		file_dest = "testersfile/thisfile";
 	}
 	
-	@And("The user is part of the group .+")
+	@And("I am a member of the group (.+)$")
 	public void set_group(){
 		group = "Testers";
 	}
 	
-	@And("The file is sent")
-	public void send_file(){
+	@Then("The group server shall confirm it recieved the file successfully")
+	public void group_server_received_file_check(){
 		UserToken token = new Token();
 		Mockito.when(m_fc.upload(file_loc, file_dest, group, token)).thenReturn(true);
 		confirm = m_fc.upload(file_loc, file_dest, group, token);
-	}
-	
-	@Then("The group server shall confirm it recieved the file successfully")
-	public void group_server_received_file_check(){
 		assertTrue(confirm.equals(true));
 	}
 	
